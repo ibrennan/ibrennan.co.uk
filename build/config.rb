a95aaec43b363f8b733f819032aec62729664ef9
Ian Brennan <ian@ibrennan.co.uk>
2017-02-17 16:28:53 +0000

    Splash page

--- build/config.rb
+++
@@ -1,9 +0,0 @@
-http_path = "../"
-css_dir = "../www/_includes/css"
-sass_dir = "sassian"
-images_dir = "images"
-http_images_path = "../images"
-environment = :development
-relative_assets = false
-output_style = :compressed
-preferred_syntax = :scss
